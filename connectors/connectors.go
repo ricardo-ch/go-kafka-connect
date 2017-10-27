564db6d8c7dac10e1fd27919419cffc44c05a4ce
Etienne Guerlais <eguerlais@gmail.com>
2017-10-27 15:12:44 +0200

    Created placeholder functions

---
+++ connectors/connectors.go
@@ -0,0 +1,51 @@
+package connectors
+
+//Create ...
+func Create() {
+
+}
+
+//Update ...
+func Update() {
+
+}
+
+//Delete ...
+func Delete() {
+
+}
+
+//Pause ...
+func Pause() {
+
+}
+
+//Resume ...
+func Resume() {
+
+}
+
+//Restart ...
+func Restart() {
+
+}
+
+//Get ...
+func Get() {
+
+}
+
+//GetAll ...
+func GetAll() {
+
+}
+
+//GetConfig ...
+func GetConfig() {
+
+}
+
+//GetStatus ...
+func GetStatus() {
+
+}
