4c498c505120ba023ec9682002b79cba73d3b68d
Etienne Guerlais <eguerlais@gmail.com>
2017-10-31 10:25:24 +0100

    Corrected getAllTasks

--- connectors/tasks.go
+++ connectors/tasks.go
@@ -1,15 +1,32 @@
 package connectors
 
+import (
+	"encoding/json"
+	"log"
+)
+
 //TaskRequest ...
 type TaskRequest struct {
 	Connector string
 	TaskID    int
 }
 
-//GetAllTasksresponse ...
-type GetAllTasksresponse struct {
+//GetAllTasksResponse ...
+type GetAllTasksResponse struct {
 	Code  int
-	Tasks []TaskConfig
+	Tasks []TaskDetails
+}
+
+//TaskDetails ...
+type TaskDetails struct {
+	ID     TaskID            `json:"id"`
+	Config map[string]string `json:"config"`
+}
+
+//TaskID ...
+type TaskID struct {
+	Connector string `json:"connector"`
+	TaskID    int    `json:"task"`
 }
 
 //TaskResponse ...
@@ -18,16 +35,24 @@ type TaskResponse struct {
 	TaskStatus map[string]string
 }
 
-//Task ...
-type TaskConfig struct {
-	Task   map[string]string `json:"id"`
-	Config map[string]string `json:"config"`
-}
-
 //GetAllTasks ...
-func (c Client) GetAllTasks(req ConnectorRequest) GetAllTasksresponse {
+func (c Client) GetAllTasks(req ConnectorRequest) GetAllTasksResponse {
+	var gatr GetAllTasksResponse
+	var taskDetails []TaskDetails
 
-	return GetAllTasksresponse{}
+	res, err := c.HTTPGet("/" + req.Name + "/tasks")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(res, &taskDetails)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gatr.Code = 200
+	gatr.Tasks = taskDetails
+	return gatr
 }
 
 //GetTaskStatus ...
