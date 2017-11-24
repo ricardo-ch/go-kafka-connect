932c2d0f770a1627a7b670e9405e2df095e67a6e
Francois Poinsot <gest.francois.poinsot@gmail.com>
2017-11-24 09:20:32 +0100

    add delete

---
+++ cli/cmd/delete.go
@@ -0,0 +1,62 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ricardo-ch/go-kafka-connect/lib/connectors"
+)
+
+type deleteCmdConfig struct {
+	connector string
+	sync      bool
+}
+
+var (
+	delete deleteCmdConfig
+)
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE:RunEDelete,
+}
+
+func RunEDelete(cmd *cobra.Command, args []string) error {
+	req := connectors.ConnectorRequest{
+		Name: delete.connector,
+	}
+
+	resp, err := connectors.NewClient(url).DeleteConnector(req, delete.sync)
+	if err != nil {
+		return err
+	}
+
+	return printResponse(resp)
+}
+
+func init() {
+	RootCmd.AddCommand(deleteCmd)
+
+	createCmd.PersistentFlags().BoolVarP(&create.sync, "sync", "y", false, "wait for asynchronous operation to be done")
+	updateCmd.PersistentFlags().StringVarP(&update.connector, "connector", "n", "", "name of connector to update")
+}
