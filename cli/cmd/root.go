e2184af87b8b7f95c24fb49868991a85aacf303c
FrancoisPoinsot <gest.francois.poinsot@gmail.com>
2017-11-26 22:51:24 +0100

    root description

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -39,16 +39,11 @@ var (
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "kafka-connect cli",
+	Long: `
+	CLI interface with kafka connect REST API
+	implement native function such as get, create, delete connectors
+	plus some other custom such as synchronous operation	`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
