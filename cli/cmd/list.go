8adee01f3a7a489b2a971808e01ca5dd4e034fd0
Francois Poinsot <gest.francois.poinsot@gmail.com>
2019-01-10 17:39:01 +0100

    add list cmd (#27)

---
+++ cli/cmd/list.go
@@ -0,0 +1,44 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List connectors",
+	RunE:  listConnectors,
+}
+
+func listConnectors(cmd *cobra.Command, args []string) error {
+	resp, err := getClient().GetAll()
+	if err != nil {
+		return err
+	}
+
+	for _, connectorName := range resp.Connectors {
+		fmt.Println(connectorName)
+	}
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+
+}
